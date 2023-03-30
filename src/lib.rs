72ee4851331fa431cd53581a49bdfe1164bd29d3
Raphael Brand <raphael-brand@users.noreply.github.com>
2023-03-30 11:57:06 +0200

    initial commit
    - credentials
    - authenticate
    - connect to database
    - login
    - get user

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![allow(dead_code, unused_variables)]
+struct Credentials {
+    username: String,
+    password: String,
+}
+
+fn authenticate(creds: Credentials) {
+    if let Status::Connected = connect_to_database() {
+        login(creds);
+    }
+}
+
+fn connect_to_database() -> Status {
+    return Status::Connected;
+}
+
+fn login(creds: Credentials) {
+    get_user();
+}
+
+fn get_user() {
+    // get user form database ...
+}
