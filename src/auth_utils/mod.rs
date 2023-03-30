53a61ebf25540b9efe468ea6382e6fb882c3a390
Raphael Brand <raphael-brand@users.noreply.github.com>
2023-03-30 14:56:59 +0200

    auth_utils module

---
+++ src/auth_utils/mod.rs
@@ -0,0 +1,16 @@
+use crate::database;
+
+pub fn login(creds: models::Credentials) {
+    database::get_user();
+}
+
+pub fn logout() {
+    // log user out ...
+}
+
+pub mod models {
+    pub struct Credentials {
+        username: String,
+        password: String,
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,12 @@
 #![allow(dead_code, unused_variables)]
 
+mod auth_utils;
 mod database;
+
+use auth_utils::*;
 use database::*;
 
-mod auth_utils {
-    use crate::database;
-
-    pub fn login(creds: models::Credentials) {
-        database::get_user();
-    }
-
-    pub fn logout() {
-        // log user out ...
-    }
-
-    pub mod models {
-        pub struct Credentials {
-            username: String,
-            password: String,
-        }
-    }
-}
-use auth_utils::models::Credentials;
-
-pub fn authenticate(creds: Credentials) {
+pub fn authenticate(creds: models::Credentials) {
     if let Status::Connected = connect_to_database() {
         auth_utils::login(creds);
     }
